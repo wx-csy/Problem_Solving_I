5a094e378c30d6d0f68af49eecbc12a3180d27ca
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2017-02-12 16:40:47 +0800

    misc changes

--- NOJ/1150.cpp
+++ NOJ/1150.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 struct record{
@@ -18,15 +19,26 @@ struct record{
         os<<rc.id<<' '<<rc.name<<' '<<rc.s1<<' '<<rc.s2<<' '<<rc.s3<<endl;
         return os;
     }
+
+    int operator <(const record& re){
+        return s1+s2+s3<re.s1+re.s2+re.s3;
+    }
 } ;
 
 int n;
 record* rcd;
+int s1s=0, s2s=0, s3s=0;
 int main(){
     cin>>n;
     rcd = new record[n];
-    for (int i=0; i<n; i++) cin>>rcd[i];
-
+    for (int i=0; i<n; i++) {
+        cin>>rcd[i];
+        s1s+=rcd[i].s1;
+        s2s+=rcd[i].s2;
+        s3s+=rcd[i].s3;
+    }
+    cout<<s1s/n<<' '<<s2s/n<<' '<<s3s/n<<endl;
+    cout<<*max_element(rcd, rcd+n);
 
     delete[] rcd;
     return 0;
